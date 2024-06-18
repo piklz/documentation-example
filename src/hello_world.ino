73201e49a61515cb9eae65a081fb7b6b6557e830
piklz <nomonkeynocircus@gmail.com>
2024-06-18 11:23:42 +0100

    Create hello_world.ino

---
+++ src/hello_world.ino
@@ -0,0 +1,12 @@
+
+void setup ()
+{      """Print info about device HELLOWORLD
+        """
+    Serial.begin (115200);
+    Serial.println ("Hello World");
+}
+
+// -----------------------------------------------------------------------------
+void loop ()
+{
+}
