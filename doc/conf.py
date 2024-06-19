0e440f0297ec8c900b552ca34a13add66fa70022
piklz <nomonkeynocircus@gmail.com>
2024-06-19 16:00:48 +0100

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -36,9 +36,9 @@ extensions = [
 
 # Breathe configuration (adjust paths as needed)
 breathe_projects = {
-    'ArduinoDocs': ('../src/', '.')  # Replace with your project name and source/header paths
+    'documentation-example': ('../src/', '.')  # Replace with your project name and source/header paths
 }
-breathe_default_group = 'ArduinoDocs'  # Replace with your project name
+breathe_default_group = 'documentation-example'  # Replace with your project name
 
 
 templates_path = ['_templates']
